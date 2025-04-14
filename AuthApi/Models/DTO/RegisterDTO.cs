130acba8f5362133ad29fe542433863d5d02d78c
Chema <josemgarciasanchz@gmail.com>
2025-04-14 15:19:26 +0200

    V 1.0 Cambios en el endpoint de requestChangePasswordAgregar archivos de proyecto.

---
+++ AuthApi/Models/DTO/RegisterDTO.cs
@@ -0,0 +1,10 @@
+﻿namespace AuthApi.Models.DTO
+{
+    public class RegisterDTO
+    {
+        public string Name { get; set; }
+        public string Password { get; set; }
+        public string PasswordRepeat { get; set; }
+        public string Email { get; set; }
+    }
+}
